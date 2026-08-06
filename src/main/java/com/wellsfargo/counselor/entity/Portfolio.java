707efbb1531b89ed54182a36724a2bd424185094
suhas-s-pai <suhaspai13@gmail.com>
2026-08-06 15:34:23 +0530

    We have implemented the data model

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,47 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @OneToOne
+    private Client client;
+
+    @OneToMany
+    private List<Security> securities;
+
+    protected Portfolio() {
+
+    }
+
+    public Portfolio(Client client, List<Security> securities) {
+        this.client = client;
+        this.securities = securities;
+    }
+
+    public Long getPortfolioId() {
+        return portfolioId;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public List<Security> getSecurities() {
+        return securities;
+    }
+
+    public void setSecurities(List<Security> securities) {
+        this.securities = securities;
+    }
+}
